0d241ace96ffb5be5901dd00f6c50fa9382780ce
arno94 <arnpat01@gmail.com>
2019-11-09 13:11:14 +0100

    basic init data

---
+++ init_data.js
@@ -0,0 +1,58 @@
+use quizApp;
+
+db.quiz.drop();
+
+db.quiz.insertOne({"_id": 0,
+  "question": "1+1?",
+  "answers":
+    [
+      "2",
+      "3",
+      "11"
+    ]
+});
+db.quiz.insertOne({"_id": 1,
+  "question": "1+1?",
+  "answers":
+    [
+      "2",
+      "3",
+      "11"
+    ]
+});
+db.quiz.insertOne({"_id": 2,
+  "question": "1+1?",
+  "answers":
+    [
+      "2",
+      "3",
+      "11"
+    ]
+});
+db.quiz.insertOne({"_id": 3,
+  "question": "1+1?",
+  "answers":
+    [
+      "2",
+      "3",
+      "11"
+    ]
+});
+db.quiz.insertOne({"_id": 4,
+  "question": "1+1?",
+  "answers":
+    [
+      "2",
+      "3",
+      "11"
+    ]
+});
+db.quiz.insertOne({"_id": 5,
+  "question": "1+1?",
+  "answers":
+    [
+      "2",
+      "3",
+      "11"
+    ]
+});
